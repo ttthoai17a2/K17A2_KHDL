7735089c85883d8cfcf9802dcc274cdf23dd1831
Khoa hoc du lieu 17A2 <ttthoai.dhkl17a2hn@sv.uneti.edu.vn>
2024-01-09 11:30:21 +0700

    Thêm file

---
+++ chao_khdl.py
@@ -0,0 +1,18 @@
+#Chương trình thứ ba
+#Tính toán số học
+print("xin mời nhập số liệu")
+x= float(input("nhập giá trị x:"))
+y= float(input("nhập giá trị y:"))
+tong = x+y
+hieu=x-y
+tich=x*y
+thuong=x/y
+print('tổng của hai số ', x, '+' , y, '=',tong)
+print('hiệu của hai số ', x, '-' , y, '=',hieu)
+print('tích của hai số ', x, '*' , y, '=',tich)
+print('thương của hai số ', x, '/' , y, '=',thuong)
+
+import math
+can_bac_hai_x=math.sqrt(x)
+print('căn bậc hai của x',x, '=', can_bac_hai_x)
+
